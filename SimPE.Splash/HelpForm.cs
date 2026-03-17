7e31f57e6b530007487fa77fda403d77f56e3797
Catherine Gramze <rhiamom@mac.com>
2026-03-16 22:59:14 -0400

    Final GUI tweaks to optionform and welcome screen

--- SimPE.Splash/HelpForm.cs
+++ SimPE.Splash/HelpForm.cs
@@ -45,8 +45,8 @@ namespace SimPe.Windows.Forms
             : base()//Color.Transparent, new Size(781, 475))
         {
             InitializeComponent();
-            this.MinimumSize = new Size(1024, 661);
-            this.MaximumSize = new Size(1024, 2048);
+            this.MinimumSize = Size.Empty;
+            this.MaximumSize = new Size(2048, 2048);
         }
 
         protected override void OnCreateBitmap(Graphics g, Bitmap b)
